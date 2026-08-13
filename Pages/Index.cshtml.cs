928591e1da9c5b1048533579399410f546c76fca
chihade <chihadben@gmail.com>
2026-08-13 19:08:01 +0200

    commit web app source codes

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webapplication1.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
